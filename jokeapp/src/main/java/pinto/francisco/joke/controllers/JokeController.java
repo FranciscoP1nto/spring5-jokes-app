12ad792e5618108ece9adbea6da7f9de27fe452e
Francisco Pinto <franciscopinto@gmai.com>
2020-02-29 22:44:45 +0000

    Anotation fixes and added Thymeleaf html template

--- jokeapp/src/main/java/pinto/francisco/joke/controllers/JokeController.java
+++ jokeapp/src/main/java/pinto/francisco/joke/controllers/JokeController.java
@@ -1,5 +1,6 @@
 package pinto.francisco.joke.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ public class JokeController {
 	
 	private JokeService jokeService;
 	
+	@Autowired
 	public JokeController(JokeService jokeService) {
 		this.jokeService = jokeService;
 	}
