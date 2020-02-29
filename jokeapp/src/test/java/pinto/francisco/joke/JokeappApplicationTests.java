13fe7ce3afb2fd1fa49bb8cd14f2e631ff58025a
Francisco Pinto <franciscopinto@gmai.com>
2020-02-29 15:04:42 +0000

    Added spring initializer project

---
+++ jokeapp/src/test/java/pinto/francisco/joke/JokeappApplicationTests.java
@@ -0,0 +1,13 @@
+package pinto.francisco.joke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
